89f81f6f7196ee03f817609271537a52e58bd5c7
Riad <riadhantouli@gmail.com>
2026-05-16 10:45:10 +0200

    update apps

--- project/helply/apps.py
+++ project/helply/apps.py
@@ -3,3 +3,14 @@ from django.apps import AppConfig
 
 class HelplyConfig(AppConfig):
     name = "helply"
+
+    def ready(self):
+        from django.db.models.signals import post_save
+        from django.contrib.auth.models import User
+        from .models import Profile
+
+        def create_profile(sender, instance, created, **kwargs):
+            if created:
+                Profile.objects.get_or_create(user=instance, defaults={'role': 'helper'})
+
+        post_save.connect(create_profile, sender=User)
