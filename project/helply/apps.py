c7295fde75038557f10218ebd1808f910d2c17a1
LAVACHE123 <louie.toulemonde@gmail.com>
2026-05-16 12:31:15 +0200

    Merge and resolve conflicts

--- project/helply/apps.py
+++ project/helply/apps.py
@@ -13,8 +13,4 @@ class HelplyConfig(AppConfig):
             if created:
                 Profile.objects.get_or_create(user=instance, defaults={'role': 'helper'})
 
-<<<<<<< Updated upstream
         post_save.connect(create_profile, sender=User)
-=======
-        post_save.connect(create_profile, sender=User)
->>>>>>> Stashed changes
